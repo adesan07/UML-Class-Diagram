6842af097fd5650648c5ecb7974c641392c543c5
Aaron <adesan07@gmail.com>
2016-10-11 18:26:49 -0600

    UML
    First user class

---
+++ UML Class Diagram/src/User.java
@@ -0,0 +1,16 @@
+
+public class User {
+
+		private String name;
+		private String userName;
+		private String password;	
+		
+	public void browseItem() {
+		
+	}
+	
+	public void register() {
+		
+	}
+
+}
